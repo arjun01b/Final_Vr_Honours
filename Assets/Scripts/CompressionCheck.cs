21dcaad248299dbd79103298f0269a7e24aaf517
arjun01bhatnagar <1904516@uad.ac.uk>
2023-03-20 17:22:13 +0000

    Compressions displaying on canvas

--- Assets/Scripts/CompressionCheck.cs
+++ Assets/Scripts/CompressionCheck.cs
@@ -3,28 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEditor.TerrainTools;
 
 public class CompressionCheck : MonoBehaviour
 {
    // public TextMeshPro ValueText;
     public float Compressions;
     public TMP_Text CompText;
+    public float time = 0;
+    public float BPM=0;
 
-    void Start()
+
+    void Update()
     {
         CompText.text = Compressions.ToString();
     }
-    private void OnTriggerExit()//Collider other)
+    private void OnTriggerExit(Collider other)
     {
        // ScoreCounter.scoreValue = ScoreCounter.scoreValue + 1;
        //
        //
-       // if (other.tag == "Left Hand" && other.tag == "Right Hand")
+        if (/*other.gameObject.tag == "Left Hand" &&*/ other.gameObject.tag == "Right Hand")
 
-       // {
+        {
             Compressions = Compressions + 1;
 
             Debug.Log(Compressions);
-       // }
+        }
     }
 }
