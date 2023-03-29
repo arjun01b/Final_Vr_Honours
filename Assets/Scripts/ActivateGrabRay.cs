40d92987a9f0d4265f6ca388d78c7ad411b285be
arjun01bhatnagar <1904516@uad.ac.uk>
2023-03-29 13:29:09 +0100

    Update ActivateGrabRay.cs

--- Assets/Scripts/ActivateGrabRay.cs
+++ Assets/Scripts/ActivateGrabRay.cs
@@ -9,6 +9,8 @@ public class ActivateGrabRay : MonoBehaviour
     public GameObject LeftGrabRay;
 
     public XRDirectInteractor leftDirectGrab;
+
+
     // Start is called before the first frame update
     void Start()
     {
