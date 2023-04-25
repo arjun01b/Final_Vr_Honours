00b27f2fa0ed2b4121a8c94050602163350013e6
arjun01bhatnagar <1904516@uad.ac.uk>
2023-04-25 14:31:57 +0100

    Exit Condition Added. Changed some canvas

---
+++ Assets/Scripts/ExitScript.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitScript : MonoBehaviour
+{
+    public void QuitGame()
+    {
+        Application.Quit();
+
+        Debug.Log("Application quit pressed");
+    }
+}
