5c1e98f559ab4a396d2ccf0cf7f9a90f1e70c472
arjun01bhatnagar <1904516@uad.ac.uk>
2023-04-27 14:59:59 +0100

    changed some stuff for exe

--- Assets/Scripts/CompressionCheck.cs
+++ Assets/Scripts/CompressionCheck.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-using UnityEditor.TerrainTools;
+//using UnityEditor.TerrainTools;
 
 public class CompressionCheck : MonoBehaviour
 {
